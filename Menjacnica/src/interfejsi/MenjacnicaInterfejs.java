6a6d3af4d301a24b4831da3d25fcd08c1976cdf0
djordjepopovic95 <djordje.popovic@outlook.com>
2016-03-17 18:53:57 +0100

    Dodata klasa ImplementacijaInterfejsa sa nezavrsenim metodama.

--- Menjacnica/src/interfejsi/MenjacnicaInterfejs.java
+++ Menjacnica/src/interfejsi/MenjacnicaInterfejs.java
@@ -1,12 +1,13 @@
 package interfejsi;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 import kursevi.Kurs;
 
 public interface MenjacnicaInterfejs {
 
-	public void dodajKurs(LinkedList<Kurs> kursevi, Kurs k);
-	public void izbrisiKurs(LinkedList<Kurs> kursevi, Kurs k);
-	public Kurs pronadjiKurs(LinkedList<Kurs> kursevi, Kurs k);
+	public LinkedList<Kurs> dodajKurs(LinkedList<Kurs> kursevi, Kurs k);
+	public LinkedList<Kurs> izbrisiKurs(LinkedList<Kurs> kursevi, Kurs k);
+	public Kurs pronadjiKurs(LinkedList<Kurs> kursevi, GregorianCalendar k);
 }
---
+++ Menjacnica/src/valute/ImplementacijaInterfejsa.java
@@ -0,0 +1,33 @@
+package valute;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import interfejsi.MenjacnicaInterfejs;
+import kursevi.Kurs;
+import valute.Valuta;
+
+public class ImplementacijaInterfejsa implements MenjacnicaInterfejs{
+
+	@Override
+	public LinkedList<Kurs> dodajKurs(LinkedList<Kurs> kursevi, Kurs k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedList<Kurs> izbrisiKurs(LinkedList<Kurs> kursevi, Kurs k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Kurs pronadjiKurs(LinkedList<Kurs> kursevi, GregorianCalendar k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+
+}

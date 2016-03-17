c45b3fb916f917c62905c490a0eb26093bc68cd8
djordjepopovic95 <djordje.popovic@outlook.com>
2016-03-17 18:13:20 +0100

    Dodate metode equals, toString, hashCode.

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -9,24 +9,71 @@ public class Valuta {
 	String naziv;
 	String skraceni_naziv;
 	LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
+
 	public String getNaziv() {
 		return naziv;
 	}
+
 	public void setNaziv(String naziv) {
 		this.naziv = naziv;
 	}
+
 	public String getSkraceni_naziv() {
 		return skraceni_naziv;
 	}
+
 	public void setSkraceni_naziv(String skraceni_naziv) {
 		this.skraceni_naziv = skraceni_naziv;
 	}
+
 	public LinkedList<Kurs> getKursevi() {
 		return kursevi;
 	}
+
 	public void setKursevi(LinkedList<Kurs> kursevi) {
 		this.kursevi = kursevi;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceni_naziv=" + skraceni_naziv + ", kursevi=" + kursevi + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceni_naziv == null) ? 0 : skraceni_naziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kursevi == null) {
+			if (other.kursevi != null)
+				return false;
+		} else if (!kursevi.equals(other.kursevi))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceni_naziv == null) {
+			if (other.skraceni_naziv != null)
+				return false;
+		} else if (!skraceni_naziv.equals(other.skraceni_naziv))
+			return false;
+		return true;
+	}
+
 }
